6a141c26d7e35c2011858b36b6b3c1c573806a83
Aaron Gray <aaron@hellometa.com>
2014-02-26 17:37:13 -0700

    Include partials for skills, articles, communities, and hero. Update styles accross all.

--- client/js/projects.js
+++ client/js/projects.js
@@ -4,8 +4,8 @@ Template.projects.featuredProjects = function () {
   return Projects.find({featured: true}, {limit: 10});
 };
 
-Template.projects.subProjects = function () {
-  return Projects.find({featured: false}, {limit: 20});
-};
+// Template.projects.subProjects = function () {
+//   return Projects.find({featured: false}, {limit: 20});
+// };
 
 Meteor.subscribe("projects");
